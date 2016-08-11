frida/v8

Fix warnings reported by MSVC for the public API

include/v8.h
@@ -4589,6 +4589,8 @@ class V8_EXPORT Extension {  // NOLINT
   virtual ~Extension() { }
   virtual v8::Handle<v8::FunctionTemplate> GetNativeFunctionTemplate(
       v8::Isolate* isolate, v8::Handle<v8::String> name) {
+    (void) isolate;
+    (void) name;
     return v8::Handle<v8::FunctionTemplate>();
   }
 
@@ -4967,7 +4969,7 @@ typedef void (*JitCodeEventHandler)(const JitCodeEvent* event);
 class V8_EXPORT ExternalResourceVisitor {  // NOLINT
  public:
   virtual ~ExternalResourceVisitor() {}
-  virtual void VisitExternalString(Handle<String> string) {}
+  virtual void VisitExternalString(Handle<String> string) { (void) string; }
 };
 
 
@@ -4978,7 +4980,10 @@ class V8_EXPORT PersistentHandleVisitor {  // NOLINT
  public:
   virtual ~PersistentHandleVisitor() {}
   virtual void VisitPersistentHandle(Persistent<Value>* value,
-                                     uint16_t class_id) {}
+                                     uint16_t class_id) {
+    (void) value;
+    (void) class_id;
+  }
 };
 
 
@@ -6290,6 +6295,7 @@ class V8_EXPORT ExtensionConfiguration {
   ExtensionConfiguration() : name_count_(0), names_(NULL) { }
   ExtensionConfiguration(int name_count, const char* names[])
       : name_count_(name_count), names_(names) { }
+  ExtensionConfiguration& operator=(const ExtensionConfiguration&);
 
   const char** begin() const { return &names_[0]; }
   const char** end()  const { return &names_[name_count_]; }
@@ -6742,6 +6748,8 @@ class Internals {
   V8_INLINE static void CheckInitialized(v8::Isolate* isolate) {
 #ifdef V8_ENABLE_CHECKS
     CheckInitializedImpl(isolate);
+#else
+    (void) isolate;
 #endif
   }
 

rjmcguire/v8-1

include/v8.h
@@ -993,6 +993,7 @@ class ScriptOriginOptions {
   V8_INLINE ScriptOriginOptions(int flags)
       : flags_(flags &
                (kIsEmbedderDebugScript | kIsSharedCrossOrigin | kIsOpaque)) {}
+  ScriptOriginOptions& operator=(const ScriptOriginOptions&);
   bool IsEmbedderDebugScript() const {
     return (flags_ & kIsEmbedderDebugScript) != 0;
   }
@@ -1025,6 +1026,7 @@ class ScriptOrigin {
       Local<Boolean> resource_is_embedder_debug_script = Local<Boolean>(),
       Local<Value> source_map_url = Local<Value>(),
       Local<Boolean> resource_is_opaque = Local<Boolean>());
+  ScriptOrigin& operator=(const ScriptOrigin&);
   V8_INLINE Local<Value> ResourceName() const;
   V8_INLINE Local<Integer> ResourceLineOffset() const;
   V8_INLINE Local<Integer> ResourceColumnOffset() const;
@@ -4742,6 +4744,8 @@ class V8_EXPORT Extension {  // NOLINT
   virtual ~Extension() { }
   virtual v8::Local<v8::FunctionTemplate> GetNativeFunctionTemplate(
       v8::Isolate* isolate, v8::Local<v8::String> name) {
+    (void) isolate;
+    (void) name;
     return v8::Local<v8::FunctionTemplate>();
   }
 
@@ -5164,7 +5168,7 @@ typedef void (*JitCodeEventHandler)(const JitCodeEvent* event);
 class V8_EXPORT ExternalResourceVisitor {  // NOLINT
  public:
   virtual ~ExternalResourceVisitor() {}
-  virtual void VisitExternalString(Local<String> string) {}
+  virtual void VisitExternalString(Local<String> string) { (void) string; }
 };
 
 
@@ -5175,7 +5179,10 @@ class V8_EXPORT PersistentHandleVisitor {  // NOLINT
  public:
   virtual ~PersistentHandleVisitor() {}
   virtual void VisitPersistentHandle(Persistent<Value>* value,
-                                     uint16_t class_id) {}
+                                     uint16_t class_id) {
+    (void) value;
+    (void) class_id;
+  }
 };
 
 
@@ -6548,6 +6555,7 @@ class V8_EXPORT ExtensionConfiguration {
   ExtensionConfiguration() : name_count_(0), names_(NULL) { }
   ExtensionConfiguration(int name_count, const char* names[])
       : name_count_(name_count), names_(names) { }
+  ExtensionConfiguration& operator=(const ExtensionConfiguration&);
 
   const char** begin() const { return &names_[0]; }
   const char** end()  const { return &names_[name_count_]; }
@@ -7005,6 +7013,8 @@ class Internals {
   V8_INLINE static void CheckInitialized(v8::Isolate* isolate) {
 #ifdef V8_ENABLE_CHECKS
     CheckInitializedImpl(isolate);
+#else
+    (void) isolate;
 #endif
   }
 